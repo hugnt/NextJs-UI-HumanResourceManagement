ca29b7653945c203fae0ae019f0a1bd10b087303
Toan211203 <toancoi123452022@gmail.com>
2024-10-24 18:55:38 +0700

    thêm giao diện cho leave application, chưa chỉnh đường dẫn sidelink, crud ok, lỗi StatusLeave value trong form-crud

---
+++ src/app/time-keeping/leave-application/form-crud.tsx
@@ -0,0 +1,222 @@
+import { Button } from "@/components/custom/button";
+import { AlertDialog, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import { CRUD_MODE } from "@/data/const";
+import { leaveApplicationSchema, StatusLeave, leaveApplicationDefault, leaveApplication } from "@/data/schema/leaveApplication.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useEffect, useState } from "react";
+import { AlertDialogDescription } from "@/components/ui/alert-dialog";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import leaveApplicationApiRequest from "@/apis/leaveApplication.api";
+import { handleErrorApi, handleSuccessApi } from "@/lib/utils";
+
+type FormProps = {
+    openCRUD: boolean,
+    mode: CRUD_MODE,
+    setOpenCRUD: (openCRUD: boolean) => void,
+    size?: number,
+    detail: leaveApplication
+}
+
+const QUERY_KEY = {
+    keyList: "leave-application",
+    keySub: "time-keeping"
+}
+
+export default function FormCRUD(props: FormProps) {
+    const { openCRUD, setOpenCRUD, size = 600, mode, detail } = props;
+    const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+    const queryClient = useQueryClient();
+
+    const addDataMutation = useMutation({
+        mutationFn: (body: leaveApplication) => leaveApplicationApiRequest.create(body),
+        onSuccess: (data) => {
+            if (data.isSuccess) {
+                queryClient.invalidateQueries({ queryKey: [QUERY_KEY.keyList] });
+                handleSuccessApi({ message: "Inserted Successfully!" });
+                setOpenCRUD(false);
+            } else {
+                handleErrorApi({ error: data.message });
+            }
+        }
+    });
+
+    const updateDataMutation = useMutation({
+        mutationFn: ({ id, body }: { id: number, body: leaveApplication }) => leaveApplicationApiRequest.update(id, body),
+        onSuccess: (data) => {
+            if (data.isSuccess) {
+                queryClient.invalidateQueries({ queryKey: [QUERY_KEY.keyList] });
+                handleSuccessApi({ message: "Updated Successfully!" });
+                setOpenCRUD(false);
+            } else {
+                handleErrorApi({ error: data.message });
+            }
+        }
+    });
+
+    const deleteDataMutation = useMutation({
+        mutationFn: (id: number) => leaveApplicationApiRequest.delete(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [QUERY_KEY.keyList] });
+            handleSuccessApi({ message: "Deleted Successfully!" });
+            setOpenCRUD(false);
+        }
+    });
+
+    const listDataEmployeeQuery = useQuery({
+        queryKey: [QUERY_KEY.keySub],
+        queryFn: () => leaveApplicationApiRequest.getAllEmployee(),
+        staleTime: 5000
+    });
+
+    const form = useForm<leaveApplication>({
+        resolver: zodResolver(leaveApplicationSchema),
+        defaultValues: mode === CRUD_MODE.ADD ? {
+            ...leaveApplicationDefault,
+            statusLeave: StatusLeave.Draft
+        } : leaveApplicationDefault,
+    });
+    const onSubmit = (data: leaveApplication) => {
+        if (mode === CRUD_MODE.ADD) addDataMutation.mutate(data);
+        else if (mode === CRUD_MODE.EDIT) updateDataMutation.mutate({ id: detail.id ?? 0, body: data });
+        else if (mode === CRUD_MODE.DELETE) deleteDataMutation.mutate(data.id ?? 0);
+    }
+
+    const handleCloseForm = (e: any) => {
+        e.preventDefault();
+        setOpenCRUD(false);
+    };
+
+    useEffect(() => {
+        if (Object.keys(detail).length > 0) {
+            form.reset(detail);
+        }
+        setIsDisabled(mode === CRUD_MODE.VIEW);
+    }, [detail, mode, openCRUD]);
+
+    return (
+        <div>
+            <AlertDialog open={openCRUD} onOpenChange={setOpenCRUD}>
+                {mode !== CRUD_MODE.DELETE ? (
+                    <AlertDialogContent className={`gap-0 top-[50%] border-none overflow-hidden p-0 sm:min-w-[500px] sm:max-w-[${size}px] !sm:w-[${size}px] sm:rounded-[0.3rem]`}>
+                        <AlertDialogHeader className='flex justify-between align-middle p-2 py-1 bg-primary'>
+                            <AlertDialogTitle className="text-slate-50">{mode === CRUD_MODE.ADD ? "Thêm yêu cầu nghỉ phép" : "Sửa yêu cầu nghỉ phép"}</AlertDialogTitle>
+                        </AlertDialogHeader>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-0">
+                                <div className="p-2 text-sm space-y-3">
+                                <FormField control={form.control} name="employeeId"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Nhân viên</FormLabel>
+                                            <FormControl>
+                                                <Select
+                                                    {...field}
+                                                    value={field.value?.toString()}
+                                                    onValueChange={field.onChange}
+                                                    disabled={isDisabled}
+                                                >
+                                                    <SelectTrigger className="w-full">
+                                                        <SelectValue placeholder="Chọn nhân viên" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        {listDataEmployeeQuery.data?.metadata?.map((item) => (
+                                                            <SelectItem key={item.id} value={item.id.toString()} style={{ color: 'black', fontSize: '1rem' }}>
+                                                                <div>
+                                                                    <p>{item.Name}</p>
+                                                                </div>
+                                                            </SelectItem>
+                                                        ))}
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />  
+
+                                    <FormField control={form.control} name="timeLeave"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Thời gian nghỉ</FormLabel>
+                                                <FormControl>
+                                                    <Input type="number" placeholder="Nhập thời gian nghỉ" {...field} disabled={isDisabled} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+
+                                    <FormField control={form.control} name="description"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Mô tả</FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder="Nhập mô tả"
+                                                        {...field}
+                                                        value={field.value ?? ''}
+                                                        disabled={isDisabled}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+
+                                    <FormField control={form.control} name="statusLeave"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Trạng thái</FormLabel>
+                                                <FormControl>
+                                                    <Select
+                                                        {...field}
+                                                        value={field.value?.toString()}
+                                                        onValueChange={field.onChange}
+                                                        disabled={isDisabled}
+                                                    >
+                                                        <SelectTrigger className="w-full">
+                                                            <SelectValue placeholder="Chọn trạng thái" />
+                                                        </SelectTrigger>
+                                                        <SelectContent>
+                                                            <SelectItem value={StatusLeave.Draft}>Draft</SelectItem>
+                                                            <SelectItem value={StatusLeave.Approved}>Approved</SelectItem>
+                                                            <SelectItem value={StatusLeave.Refuse}>Refuse</SelectItem>
+                                                        </SelectContent>
+                                                    </Select>
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <AlertDialogFooter className="p-2 py-1 bg-secondary/80">
+                                    <Button onClick={handleCloseForm} className="bg-gray-400 hover:bg-red-500" size='sm'>Close</Button>
+                                    {(mode === CRUD_MODE.ADD || mode === CRUD_MODE.EDIT) &&
+                                        <Button type="submit" size='sm'>Save</Button>}
+                                </AlertDialogFooter>
+                            </form>
+                        </Form>
+                    </AlertDialogContent>
+                ) : (
+                    <AlertDialogContent className={`gap-0 top-[50%] border-none overflow-hidden p-0 w-[400px] sm:rounded-[0.3rem]`}>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Xác nhận xóa</AlertDialogTitle>
+                        </AlertDialogHeader>
+                        <AlertDialogDescription className="text-center pb-4 text-lg text-stone-700">
+                            Bạn có chắc chắn muốn xóa yêu cầu này không?
+                        </AlertDialogDescription>
+                        <AlertDialogFooter className="!justify-center p-2 py-3 text-center">
+                            <Button onClick={handleCloseForm} className="bg-gray-400 hover:bg-red-500" size='sm'>Close</Button>
+                            <Button className="" size='sm' onClick={() => onSubmit(detail)}>Confirm</Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                )}
+            </AlertDialog>
+        </div>
+    );
+}
---
+++ src/app/time-keeping/leave-application/page.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import leaveApplicationApiRequest from "@/apis/leaveApplication.api";
+import FormCRUD from "@/app/time-keeping/leave-application/form-crud";
+import AppBreadcrumb, { PathItem } from "@/components/custom/_breadcrumb";
+import { Button } from "@/components/custom/button";
+import { DataTable, DataTableColumnHeader, DataTableRowActions } from "@/components/data-table";
+import { useQuery } from "@tanstack/react-query";
+import { ColumnDef, Row } from '@tanstack/react-table';
+import { useState } from "react";
+import { CRUD_MODE } from "@/data/const";
+import { leaveApplication } from "@/data/schema/leaveApplication.schema";
+import { DataFilter } from "@/components/data-table/data-table-toolbar";
+import { IconPlus } from "@tabler/icons-react";
+
+const pathList: Array<PathItem> = [
+  {
+    name: "Time keeping",
+    url: "/time-keeping"
+  },
+  {
+    name: "Leave applications",
+    url: "/time-keeping/leave-applications"
+  },
+];
+
+const dataFilter: Array<DataFilter> = [
+  {
+    columnName: 'statusLeave',
+    title: 'Status Leave',
+    options: [
+      {
+        label: 'Draft',
+        value: 'Draft'
+      },
+      {
+        label: 'Approved',
+        value: 'Approved'
+      },
+      {
+        label: 'Refuse',
+        value: 'Refuse'
+      }
+    ],
+  },
+];
+
+export default function LeaveApplicationList() {
+  const [detail, setDetail] = useState<leaveApplication>({});
+  const [openCRUD, setOpenCRUD] = useState<boolean>(false);
+  const [mode, setMode] = useState<CRUD_MODE>(CRUD_MODE.VIEW);
+
+  const listDataQuery = useQuery({
+    queryKey: ["leave-application"],
+    queryFn: () => leaveApplicationApiRequest.getList(),
+  });
+
+  const columnsDef: ColumnDef<leaveApplication>[] = [
+    {
+      accessorKey: 'employeeId',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Employee ID' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('employeeId')}</div>,
+    },
+    {
+      accessorKey: 'timeLeave',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Time Leave' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('timeLeave')}</div>,
+    },
+    {
+      accessorKey: 'description',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Description' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('description')}</div>,
+    },
+    {
+      accessorKey: 'statusLeave',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Status Leave' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('statusLeave')}</div>,
+    },
+    {
+      accessorKey: 'replyMessage',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Reply Message' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('replyMessage')}</div>,
+    },
+    {
+      accessorKey: 'refuseReason',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Refuse Reason' />
+      ),
+      cell: ({ row }) => <div>{row.getValue('refuseReason')}</div>,
+    },
+    {
+      id: 'actions',
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title='Action' />
+      ),
+      cell: ({ row }) => (
+        <DataTableRowActions
+          row={row}
+          handleView={() => handleView(row)}
+          handleEdit={() => handleEdit(row)}
+          handleDelete={() => handleDelete(row)}
+        />
+      ),
+    },
+  ];
+
+  // ACTION HANDLER
+  const handleAddNew = () => {
+    setDetail({});
+    setMode(CRUD_MODE.ADD);
+    setOpenCRUD(true);
+  };
+
+  const handleView = (row: Row<leaveApplication>) => {
+    const id = row.original.id;
+    setMode(CRUD_MODE.VIEW);
+    const selectedData = listDataQuery.data?.metadata?.find(x => x.id === id) ?? {};
+    setDetail(selectedData);
+    setOpenCRUD(true);
+  };
+
+  const handleEdit = (row: Row<leaveApplication>) => {
+    const id = row.original.id;
+    setMode(CRUD_MODE.EDIT);
+    const selectedData = listDataQuery.data?.metadata?.find(x => x.id === id) ?? {};
+    setDetail(selectedData);
+    setOpenCRUD(true);
+  };
+
+  const handleDelete = (row: Row<leaveApplication>) => {
+    const id = row.original.id;
+    setMode(CRUD_MODE.DELETE);
+    const selectedData = listDataQuery.data?.metadata?.find(x => x.id === id) ?? {};
+    setDetail(selectedData);
+    setOpenCRUD(true);
+  };
+
+  return (
+    <>
+      <div className='mb-2 flex items-center justify-between space-y-2'>
+        <div>
+          <h2 className='text-2xl font-bold tracking-tight'>Leave Application List</h2>
+          <AppBreadcrumb pathList={pathList} className="mt-2" />
+        </div>
+        <Button onClick={handleAddNew} variant='outline' size='sm' className='bg-primary text-white'>
+          Add New
+        </Button>
+      </div>
+
+      <div className='overflow-auto'>
+        <DataTable data={listDataQuery.data?.metadata} columns={columnsDef} filters={dataFilter} searchField="name" />
+      </div>
+
+      <FormCRUD openCRUD={openCRUD} setOpenCRUD={setOpenCRUD} mode={mode} detail={detail} />
+    </>
+  );
+}
